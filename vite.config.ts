26f0cad973b5affee5741335b8df1dfad4d9e649
代鹏 <daipeng@seeyon.com>
2021-12-05 22:25:11 +0800

    fix(versionrc): fix version.js

--- vite.config.ts
+++ vite.config.ts
@@ -8,52 +8,52 @@ const resolve = (...list: string[]) => path.resolve(__dirname, ...list);
 const includesStyleReg = new RegExp(`^${resolve('src')}`);
 const includesStylePaths = (filename) => includesStyleReg.test(filename);
 const additionalData = (source, filename) => {
-	if (includesStylePaths(filename)) return `@import "@/style/core/_index.scss"; \n ${source}`;
-	return source;
+    if (includesStylePaths(filename)) return `@import "@/style/core/_index.scss"; \n ${source}`;
+    return source;
 };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	server: {
-		host: '127.0.0.1',
-		port: 8900,
-		open: true,
-		proxy: {
-			'/api': {
-				target: 'http://127.0.0.1:8089',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
-			},
-		},
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				additionalData,
-			},
-			sass: {
-				additionalData,
-			},
-		},
-	},
-	plugins: [
-		rollupPluginIconfont({
-			svgs: resolve('src/assets/svgs/*.svg'),
-			fontsOutput: resolve('src/style/iconfont/fonts'),
-			cssOutput: resolve('src/style/iconfont/index.css'),
-			fontName: 'custom-iconfont',
-			jsOutput: false,
-			htmlOutput: false,
-			template: 'scss',
-			cssFontPath: '@/style/iconfont/fonts',
-			cssPrefix: 'ift',
-		}),
-		vue(),
-	],
-	resolve: {
-		alias: {
-			vue: 'vue/dist/vue.esm-bundler.js',
-			'@': resolve('./src'),
-		},
-	},
+    server: {
+        host: '127.0.0.1',
+        port: 8900,
+        open: true,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8089',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData,
+            },
+            sass: {
+                additionalData,
+            },
+        },
+    },
+    plugins: [
+        rollupPluginIconfont({
+            svgs: resolve('src/assets/svgs/*.svg'),
+            fontsOutput: resolve('src/style/iconfont/fonts'),
+            cssOutput: resolve('src/style/iconfont/index.css'),
+            fontName: 'custom-iconfont',
+            jsOutput: false,
+            htmlOutput: false,
+            template: 'scss',
+            cssFontPath: '@/style/iconfont/fonts',
+            cssPrefix: 'ift',
+        }),
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+            '@': resolve('./src'),
+        },
+    },
 });
